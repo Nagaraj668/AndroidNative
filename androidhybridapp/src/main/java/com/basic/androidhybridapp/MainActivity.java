87096153c47caf82a644704dd4ba0b1c60cf6439
NAGARAJ SRJ <nnagarajece31@gmail.com>
2017-08-10 22:57:45 +0530

    Action bar removed

--- androidhybridapp/src/main/java/com/basic/androidhybridapp/MainActivity.java
+++ androidhybridapp/src/main/java/com/basic/androidhybridapp/MainActivity.java
@@ -1,7 +1,12 @@
 package com.basic.androidhybridapp;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView) findViewById(R.id.webview);
+        webView.loadUrl("file:///android_asset/www/index.html");
+        webView.addJavascriptInterface(new WebAppInterface(this), "android");
+
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
+
+    public class WebAppInterface {
+        Context mContext;
+
+        /** Instantiate the interface and set the context */
+        WebAppInterface(Context c) {
+            mContext = c;
+        }
+
+        /** Show a toast from the web page */
+        @JavascriptInterface
+        public void showToast(String toast) {
+            Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
+        }
     }
 }
